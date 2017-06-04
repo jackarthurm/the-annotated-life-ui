8ceec6b0a7ba9c73aa640f0cc18508cb74aa5b10
Jack Mann <jack@localhost.localdomain>
2017-06-04 18:04:22 +0200

    Improved post list component and styling

--- src/view/post-list/post-list.view.js
+++ src/view/post-list/post-list.view.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from '../../logo.svg';
 import './post-list.view.css';
+
+import React from 'react';
 
 import PostView from '../post/post.view';
 
@@ -11,16 +11,15 @@ function PostListView(props) {
 
   const postList = posts.map(
 
-    post => <li key={post.uuid}><PostView post={post}/></li>
+    post => <li key={post.uuid} 
+                className="PostListView-item flex-item">
+              <PostView post={post} />
+            </li>
   );
 
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to The Annotated Life</h2>
-      </div>
-      <ul>
+    <div className="PostListView">
+      <ul className="PostListView-list flex-container">
         {postList}
       </ul>
     </div>
