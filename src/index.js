d782fe6c2da01e2a324d5d937bfe92508e2f12ce
Jack Mann <jack@localhost.localdomain>
2017-06-04 18:00:22 +0200

    Added web fonts

--- src/index.js
+++ src/index.js
@@ -8,11 +8,34 @@ import {
 } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import AppContainer from './containers/app.container';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import rootReducer from './reducers';
 
+import WebFont from 'webfontloader';
+
+const TITLE_TEXT = 'TheAnnotatedLife';
+
+
+WebFont.load({
+  google: {
+    families: ['Cinzel Decorative:400:latin'],
+    text: TITLE_TEXT
+  }
+});
+
+WebFont.load({
+  google: {
+    families: ['Alegreya:400:latin', 
+               'Alegreya SC:400:latin']
+  }
+});
+
+
+injectTapEventPlugin();
 
 const store = createStore(
   rootReducer,
@@ -20,7 +43,6 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  
   <Provider store={store}>
 		<Router>
       <AppContainer />
