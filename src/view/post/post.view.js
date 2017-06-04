49e0be99f65858e877adbebd55f1987733fe9c14
Jack Mann <jack@localhost.localdomain>
2017-06-04 18:04:49 +0200

    Improved single post component and styling

--- src/view/post/post.view.js
+++ src/view/post/post.view.js
@@ -1,13 +1,38 @@
+import './post.view.css';
+
 import React from 'react';
+import moment from 'moment/moment';
+
+
+const DATE_FORMAT = "dddd, MMMM Do YYYY, h:mm a";
+const DATE_INVALID = ''
+
 
 function PostView(props) {
 
-  const post = props.post;
+  const {title, body, pub_date} = props.post;
+
+  let published = moment(pub_date);
+
+  if (published.isValid()) {
+    published = published.fromNow();
+  }
+  else {
+    published = DATE_INVALID
+  }
 
   return (
     <div className="Post">
-      <h1 className="Post-title">{post.title}</h1>
-      <p className="Post-body">{post.body}</p>
+      <div className='Post-header'>
+        <h2 className="Post-title">{title}</h2>
+        <p className="Post-date">{published}</p>
+      </div>
+      <div className='Post-body'>
+        <p className="Post-snippet">{body}</p>
+      </div>
+      <div className='Post-footer'>
+        <p>Footer?</p>
+      </div>
     </div>
   );
 }
