728b35737ff6366afd9a421884d57b1360c9fd0f
Jack Mann <jack.mann@romaxtech.com>
2020-05-21 22:35:24 +0100

    Used API data instead of fixtures

--- src/services/posts-service.ts
+++ src/services/posts-service.ts
@@ -3,22 +3,21 @@ import { nonAbortablefetch, HttpMethod } from '../http';
 import {
   IPostSummaryResourceList,
   IPostSummaryResource,
-  IPostResource,
 } from '../models/resources';
-import { uuidString } from '../models/types';
+import { uuidString, urlString } from '../models/types';
 
-import postFixtures from '../data/post-summary-fixtures.json';
+const url: urlString = 'https://the-annotated-life-api.herokuapp.com/';
 
 export class PostService {
   public static async getPostSummaryList(): Promise<IPostSummaryList> {
     const response: Response = await nonAbortablefetch(
-      'https://the-annotated-life-api.herokuapp.com/posts/',
+      `${url}posts/`,
       HttpMethod.GET,
       undefined,
       new Headers({ Accept: 'application/json' })
     );
 
-    const postSummariesListResource: IPostSummaryResourceList = postFixtures as IPostSummaryResourceList;
+    const postSummariesListResource: IPostSummaryResourceList = await response.json();
 
     return {
       posts: postSummariesListResource.posts.map(
@@ -36,34 +35,13 @@ export class PostService {
   }
 
   public static async getPostDetail(objectID: uuidString): Promise<IPost> {
-    // const response: Response = await nonAbortablefetch(
-    //   `https://the-annotated-life-api.herokuapp.com/posts/${objectID}/`,
-    //   HttpMethod.GET,
-    //   undefined,
-    //   new Headers({ Accept: 'application/json' })
-    // );
+    const response: Response = await nonAbortablefetch(
+      `${url}posts/${objectID}/`,
+      HttpMethod.GET,
+      undefined,
+      new Headers({ Accept: 'application/json' })
+    );
 
-    const postResource: IPostResource = {
-      object_id: '16fd2706-8baf-433b-82eb-8c7fada847da',
-      object_url: '16fd2706-8baf-433b-82eb-8c7fada847da',
-      title_text: 'Post number one',
-      date_published: '2020-05-07',
-      date_written: '2020-05-07',
-      body_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      footer_text: "I'm a footer",
-      author_url: 'https://author.com',
-      author_id: '16fd2706-8baf-433b-82eb-8c7fada847da',
-      tags: ['tag1', 'tag2'],
-      references: [],
-      summary_text: '',
-    };
-
-    return {
-      uuid: postResource.object_id,
-      title: postResource.title_text,
-      body: postResource.body_text,
-      pubDate: postResource.date_published,
-      footer: postResource.footer_text,
-    };
+    return await response.json();
   }
 }
